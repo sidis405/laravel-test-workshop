90d04827d4e3b7dcd99121eb08750009de249037
Sidrit Trandafili <forge405@gmail.com>
2019-07-02 12:53:42 +0200

    Comit damn it

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,6 @@ Route::get('posts/create', '\WS\Http\Controllers\PostsController@create')->name(
 Route::get('posts/{post}', '\WS\Http\Controllers\PostsController@show')->name('posts.show');
 Route::post('posts', '\WS\Http\Controllers\PostsController@store')->name('posts.store');
 
-//
-
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
